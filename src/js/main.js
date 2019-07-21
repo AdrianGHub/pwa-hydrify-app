061d26b7e3f7905f2319f488e266817d57f46c25
Adrian <adrian_zinko@op.pl>
2019-07-22 00:17:15 +0200

    feat: implement localstorage usage

--- src/js/main.js
+++ src/js/main.js
@@ -29,16 +29,26 @@ const counter = document.querySelector(".glass__counter--js");
 const addButton = document.querySelector(".glass__button--add--js");
 const removeButton = document.querySelector(".glass__button--remove--js");
 
+const key = new Date().toISOString().slice(0, 10);
+
 let count = 0;
 let opacity = 0.5;
 
-addButton.addEventListener("click", () => {
+addButton.addEventListener("click", e => {
   if (count >= 10) {
     counter.innerHTML = "MAX";
     count = 10;
   } else {
     count += 1;
     counter.innerHTML = count;
+
+    // LOCALSTORAGE
+    if (localStorage.getItem("key")) {
+      localStorage.getItem("key");
+    }
+    localStorage.setItem("key", count);
+
+    // OPACITY
     if (opacity <= 0.5) opacity += 0.05;
     else {
       opacity = 0.5;
@@ -46,12 +56,20 @@ addButton.addEventListener("click", () => {
   }
 });
 
-removeButton.addEventListener("click", () => {
+removeButton.addEventListener("click", e => {
   if (count <= 0) {
     count = 0;
   } else {
     count -= 1;
     counter.innerHTML = count;
+
+    // LOCALSTORAGE
+    if (localStorage.getItem("key")) {
+      localStorage.getItem("key");
+    }
+    localStorage.setItem("key", count);
+
+    // OPACITY
     if (opacity >= 0.5) opacity -= 0.05;
     else {
       opacity = 0.5;
