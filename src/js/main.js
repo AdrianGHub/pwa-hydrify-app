f8c24c0e213974c6a82462d9b1754b106ebf347f
Adrian <adrian_zinko@op.pl>
2019-07-21 21:41:49 +0200

    create query selectors for interactive elements

--- src/js/main.js
+++ src/js/main.js
@@ -2,21 +2,28 @@
 
 // service worker registration - remove if you're not going to use it
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker.register("serviceworker.js").then(
+      function(registration) {
+        // Registration was successful
+        console.log(
+          "ServiceWorker registration successful with scope: ",
+          registration.scope
+        );
+      },
+      function(err) {
+        // registration failed :(
+        console.log("ServiceWorker registration failed: ", err);
+      }
+    );
   });
 }
 
 // place your code below
 
+console.log(`Drink Up! 💙`);
 
-console.log(`Hello world!`);
-
-
+const counter = document.querySelector(".glass__counter");
+const addButton = document.querySelector(".glass__button--add");
+const removeButton = document.querySelector(".glass__button--remove");
