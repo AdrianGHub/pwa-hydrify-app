1b6486d844dbd3934794aa06a745739832fa714d
Adrian <adrian_zinko@op.pl>
2019-07-21 23:10:28 +0200

    create counter event listeners

--- src/js/main.js
+++ src/js/main.js
@@ -24,6 +24,39 @@ if ("serviceWorker" in navigator) {
 
 console.log(`Drink Up! 💙`);
 
-const counter = document.querySelector(".glass__counter");
-const addButton = document.querySelector(".glass__button--add");
-const removeButton = document.querySelector(".glass__button--remove");
+const image = document.querySelector(".glass__image--js");
+const counter = document.querySelector(".glass__counter--js");
+const addButton = document.querySelector(".glass__button--add--js");
+const removeButton = document.querySelector(".glass__button--remove--js");
+
+let count = 0;
+let opacity = 0.5;
+
+addButton.addEventListener("click", () => {
+  if (count >= 10) {
+    counter.innerHTML = "MAX";
+    count = 10;
+  } else {
+    count += 1;
+    counter.innerHTML = count;
+    if (opacity <= 0.5) opacity += 0.05;
+    else {
+      opacity = 0.5;
+    }
+  }
+});
+
+removeButton.addEventListener("click", () => {
+  if (count <= 0) {
+    count = 0;
+  } else {
+    count -= 1;
+    counter.innerHTML = count;
+    if (opacity >= 0.5) opacity -= 0.05;
+    else {
+      opacity = 0.5;
+    }
+  }
+});
+
+image.style.opacity = opacity;
