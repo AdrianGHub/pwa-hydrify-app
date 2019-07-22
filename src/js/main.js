b6ad4340dd84ab706279167d5db28d17b03c20cf
Adrian <adrian_zinko@op.pl>
2019-07-22 11:24:51 +0200

    add event listeners with locastorage

--- src/js/main.js
+++ src/js/main.js
@@ -25,8 +25,34 @@ if ("serviceWorker" in navigator) {
 console.log(`Drink Up! 💙`);
 
 const image = document.querySelector(".glass__image--js");
-const counter = document.querySelector(".glass__counter--js");
+const value = document.querySelector(".glass__counter--js");
 const addButton = document.querySelector(".glass__button--add--js");
 const removeButton = document.querySelector(".glass__button--remove--js");
 
 const key = new Date().toISOString().slice(0, 10);
+
+// LOCAL STORAGE
+
+if (!localStorage.getItem(key)) {
+  localStorage.setItem(key, 0);
+  value.innerHTML = "0";
+} else {
+  value.innerHTML = localStorage.getItem(key);
+}
+
+// ADD VALUE LISTENER
+
+addButton.addEventListener("click", e => {
+  localStorage.setItem(key, parseInt(localStorage.getItem(key)) + 1);
+  value.innerHTML = localStorage.getItem(key);
+});
+
+// REMOVE VALUE LISTENER
+
+removeButton.addEventListener("click", e => {
+  const currentCounter = parseInt(localStorage.getItem(key));
+  if (currentCounter > 0) {
+    localStorage.setItem(key, parseInt(localStorage.getItem(key)) - 1);
+    value.innerHTML = localStorage.getItem(key);
+  }
+});
