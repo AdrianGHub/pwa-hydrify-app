6dd391c0ade9a7f1ae93ca6896c5e8eab377ee63
Adrian <adrian_zinko@op.pl>
2019-07-22 11:28:50 +0200

    feat: update buttons class name

--- src/js/main.js
+++ src/js/main.js
@@ -26,8 +26,8 @@ console.log(`Drink Up! 💙`);
 
 const image = document.querySelector(".glass__image--js");
 const value = document.querySelector(".glass__counter--js");
-const addButton = document.querySelector(".glass__button--add--js");
-const removeButton = document.querySelector(".glass__button--remove--js");
+const addButton = document.querySelector(".glass__button--add-js");
+const removeButton = document.querySelector(".glass__button--remove-js");
 
 const key = new Date().toISOString().slice(0, 10);
 
